bc4fc3e5c72570bac22165484963cbcb6e1feb2f
Daniela <remenska@gmail.com>
2013-09-25 02:37:31 +0200

    zzz

--- EclipsePlugins/dr/info.remenska.myFirstPlugin/src/Test2.java
+++ EclipsePlugins/dr/info.remenska.myFirstPlugin/src/Test2.java
@@ -1,5 +1,9 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
-
+import java.util.Queue;
+//import org.eclipse.core.internal.utils.Queue;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -37,25 +41,44 @@ public class Test2 {
 			TreeNode<String> quest111 = answ11.addChild("Which of the following choices best describes the restricted interval(s)?", true);
 				TreeNode<String> answ1111 = quest111.addChild("There can be at most one restricted interval ", false);
 				TreeNode<String> answ1112 = quest111.addChild("Can have both blabla ", false);
+				TreeNode<String> answ1113 = quest111.addChild("Anything can happen hello ", false);
 		TreeNode<String> answ12 = questionnaire.addChild("No, the behavior is required to hold throughout the entire event sequence", false);
 			TreeNode<String> quest121 = answ11.addChild("Really are you sure? ", true);
 				TreeNode<String> answ1211 = quest121.addChild("Blaaah yah1", false);
 				TreeNode<String> answ1212 = quest121.addChild("Blaaah yah2", false);
-				
-				
+		   TreeNode<String> quest122 = answ11.addChild("Really are you sure sure? ", true);
+				TreeNode<String> answ1221 = quest122.addChild("Blaaah blaah yah1", false);
+				TreeNode<String> answ1222 = quest122.addChild("Blaaah blaah yah2", false);		
+				aman = 1;
 				
 			    // traverse?
-		TreeNode<String > treeRoot = questionnaire;
-		for(TreeNode<String> node: treeRoot) {
+//		TreeNode<String > treeRoot = questionnaire;
+//		for(TreeNode<String> node: treeRoot) {
+//			String ident = createIndent(node.getLevel());
+//			String hmmm = node.isQuestion?"Q: ":"A: ";
+//			
+////			System.out.println(ident+hmmm+node.data + " "+aman); // <-- so it's depth first
+//			aman++;
+//		}
+				traverseBFS(questionnaire);
+	}
+	
+	public static void traverseBFS(TreeNode root){
+		root = questionnaire;
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		while(!queue.isEmpty()){
+			TreeNode node = (TreeNode) queue.remove();
 			String ident = createIndent(node.getLevel());
 			String hmmm = node.isQuestion?"Q: ":"A: ";
-			
+			queue.addAll(node.children);
+//			queue.add(node.children);
 			System.out.println(ident+hmmm+node.data + " "+aman); // <-- so it's depth first
 			aman++;
 		}
+		
 	}
 	
-
 	public static void main(String[] args) {
 		createQuestionnaire();
 		Display display = new Display();
@@ -90,142 +113,44 @@ public class Test2 {
 			}
 
 		};
-		
-	    // traverse?
-//		TreeNode<String > treeRoot = questionnaire;
-//		for(TreeNode<String> node: treeRoot) {
-//			String ident = createIndent(node.getLevel());
-//			String hmmm = node.isQuestion?"Q: ":"A: ";
-//			ExpandBar expandBar1 = new ExpandBar(shell, SWT.NONE);;
-//			Composite composite1 = new Composite(expandBar1, SWT.NONE);;
-//			if(node.isQuestion){
-//				 expandBar1 = new ExpandBar(shell, SWT.NONE);
-//				 composite1 = new Composite(expandBar1, SWT.NONE);
-//					GridLayout layout1 = new GridLayout(1, true);
-//					composite1.setLayout(layout1);
-//				 ExpandItem expandItem1 = new ExpandItem(expandBar1, SWT.NONE, 0);
-//				expandItem1.setText(node.data);
-//				expandItem1.setHeight(node.getLevel()*200);
-//
-//			} else{
-//				Button label = new Button(composite1, SWT.RADIO);
-//				label.setText(node.data);
-//				label.addSelectionListener(selectionListener);
-//			}
-//			
-//			System.out.println(ident+hmmm+node.data);
-//			
-//			
-//		}
-		
-//		ExpandBar expandBar1 = new ExpandBar(shell, SWT.NONE);
-//
-//		expandBar1.setLayoutData(new GridData(GridData.FILL, GridData.FILL,
-//				true, true, 2, 1));
-//		Composite composite1 = new Composite(expandBar1, SWT.NONE);
-//		GridLayout layout1 = new GridLayout(1, true);
-//		composite1.setLayout(layout1);
-//		composite1.setVisible(true);
-		TreeNode<String > treeRoot = questionnaire;
-		// Question?
-//		ExpandItem expandItem1 = new ExpandItem(expandBar1, SWT.NONE, 0);
-//		expandItem1
-//				.setText(questionnaire.data);
-//		expandItem1 
-//				.setHeight(composite1.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-//		expandItem1.setControl(composite1);
-		Stack<ExpandBar> expandBars = new Stack<ExpandBar>();
-		Stack<ExpandItem> expandItems = new Stack<ExpandItem>();
-		Stack<Composite> composites = new Stack<Composite>();
-		for(TreeNode<String> node: treeRoot) {
+
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(questionnaire);
+		while(!queue.isEmpty()){
+			TreeNode node = (TreeNode) queue.remove();
+
+			ExpandBar expandBar1= new ExpandBar(shell, SWT.NONE);;
+			Composite composite1 = new Composite(expandBar1, SWT.NONE);;
+			ExpandItem expandItem1;
+			Button label1;
 			if (node.isQuestion){
-				expandBars.add(new ExpandBar(shell, SWT.NONE));
-				expandBars.peek().setLayoutData(new GridData(GridData.FILL, GridData.FILL,
-						true, true, 2, 1));
-				composites.add(new Composite(expandBars.peek(), SWT.NONE));
-				GridLayout layout1 = new GridLayout(2, true);
-				composites.peek().setLayout(layout1);
-				expandItems.add(new ExpandItem(expandBars.peek(), SWT.NONE, 0));
-				expandItems.peek().setText(node.data);
-				expandItems.peek().setHeight(300+expandItems.size()*300);
-				expandItems.peek().setControl(composites.peek());
-			} else if (!node.isQuestion){
-				Button label1 = new Button(composites.peek(), SWT.RADIO);
-				label1.setText(node.data);
-				label1.addSelectionListener(selectionListener);
-			}
+			
+//				if(queue.isEmpty()){
+					expandBar1 = new ExpandBar(shell, SWT.NONE);
+					expandBar1.setLayoutData(new GridData(GridData.FILL, GridData.FILL,true, true, 2, 1));
+					composite1 = new Composite(expandBar1, SWT.NONE);
+
+//				}
+				GridLayout layout1 = new GridLayout(1, true);
+				composite1.setLayout(layout1);
+
+				expandItem1 = new ExpandItem(expandBar1, SWT.NONE, 0);
+				expandItem1.setText((String) node.data);
+				expandItem1.setHeight(100);
+				expandItem1.setControl(composite1);
+
+				List<TreeNode<String>> deca = node.children;
+				for(TreeNode dete:deca){
+					label1 = new Button(composite1, SWT.RADIO);
+					label1.setText((String) dete.data);
+					label1.addSelectionListener(selectionListener);
+					queue.addAll(dete.children);
+				}
+			} 
 
 		}
-//		Button label1 = new Button(composite1, SWT.RADIO);
-//		label1.setText("Yes, the behavior is only required to hold within restricted interval(s) in the event sequence");
-//		Button label2 = new Button(composite1, SWT.RADIO);
-//		label2.setText("No, the behavior is required to hold throughout the entire event sequence");
-//
-//		final ExpandItem expandItem1 = new ExpandItem(expandBar1, SWT.NONE, 0);
-//		expandItem1
-//				.setText("Is the behavior only required to hold within the restricted interval(s) in the event sequence?   ");
-//		expandItem1
-//				.setHeight(composite1.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-//		expandItem1.setControl(composite1);
-//
-//		/**
-//		 * ExpandBar secondaire
-//		 */
-//		final ExpandBar expandBar2 = new ExpandBar(composite1, SWT.NONE);
-//		expandBar2.setLayoutData(new GridData(GridData.FILL, GridData.FILL,
-//				true, true, 2, 1));
-//
-//		final Composite composite2 = new Composite(expandBar2, SWT.NONE);
-//		composite2.setLayout(new GridLayout(2, true));
-//		expandBar2.setVisible(false);
-//
-//		Button labelSec = new Button(composite2, SWT.RADIO);
-//		labelSec.setText("SWT.ICON_INFORMATION");
-//		labelSec.addSelectionListener(selectionListener);
-//		labelSec = new Button(composite2, SWT.RADIO);
-//		labelSec.setText("SWT.ICON_INFORMATION_1");
-//		labelSec.addSelectionListener(selectionListener);
-//
-//		final ExpandItem expandItem2 = new ExpandItem(expandBar2, SWT.NONE, 0);
-//		expandItem2
-//				.setText("How many events of primary interest are there in this behavior?    ");
-//		expandItem2.setControl(composite2);
-//		expandItem2.setExpanded(false);
-//		final ExpandBar expandBar3 = new ExpandBar(composite2, SWT.NONE);
-//		expandBar3.setLayoutData(new GridData(GridData.FILL, GridData.FILL,
-//				true, true, 2, 1));
-//		final Composite composite3 = new Composite(expandBar3, SWT.NONE);
-//		composite3.setLayout(new GridLayout(2, true));
-//		expandBar3.setVisible(false);
-//		Button labelThird = new Button(composite3, SWT.RADIO);
-//		labelThird.addSelectionListener(selectionListener);
-//
-//		labelThird.setText("Third level  ");
-//		labelThird = new Button(composite3, SWT.RADIO);
-//		labelThird.setText("Third level yaah  ");
-//		labelThird.addSelectionListener(selectionListener);
-//
-//		final ExpandItem expandItem3 = new ExpandItem(expandBar3, SWT.NONE, 0);
-//		expandItem3.setText("Yet another expand item");
-//		expandItem3.setControl(composite3);
-//		expandItem3.setExpanded(false);
+		
 
-//		int heightItem3 = composite3.computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
-//		int heightItem2 = composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y
-//				+ heightItem3;
-//		int heightItem1 = composite1.computeSize(SWT.DEFAULT, SWT.DEFAULT).y
-//				+ heightItem2;
-//		System.out.println(heightItem1 + "  " + heightItem2 + "  "
-//				+ heightItem3);
-//		expandItem1.setHeight(heightItem1);
-//
-//		expandItem2.setHeight(heightItem2);
-//		expandItem3.setHeight(heightItem3);
-//
-//		// Fin d'implémentation du test
-//
-//		label1.addSelectionListener(selectionListener);
-//		label2.addSelectionListener(selectionListener);
 
 		shell.open();
 		while (!shell.isDisposed()) {
